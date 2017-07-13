fbfa9707582be3caddf6274db6e3cd815fd7d6db
oliver chen <olichen@ucdavis.edu>
2017-07-13 13:17:35 -0400

    fixed cmake double compiling source files for tests

--- test/resources_test.cpp
+++ test/resources_test.cpp
@@ -1,13 +1,13 @@
 #include <catch.hpp>
 #include "resources.h"
 
-unsigned int Factorial( unsigned int number ) {
-	return number <= 1 ? number : Factorial(number-1)*number;
+TEST_CASE("resources initialize properly", "[init]") {
+	Resources resources;
+
+	SECTION("test a") {
+		CHECK(resources.getMinerals() == 0);
+
+	}
+//#: mMinerals(0), mGas(0), mSupply(0), mSupplyMax(0), mFrame(0), mMineralPatches(mineralPatches), mGasGeysers(gasGeysers), mRace(race)
 }
 
-TEST_CASE ( "Factorials are computed", "[factorial]" ) {
-	REQUIRE(Factorial(1) == 2);
-	REQUIRE(Factorial(2) == 2);
-	REQUIRE(Factorial(3) == 6);
-	REQUIRE(Factorial(10) == 3628800);
-}

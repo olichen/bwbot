2679da2f7e64a691554ab8a2feb33132fbf14eb7
Oliver Chen <olichen@ucdavis.edu>
2020-12-03 21:42:32 -0800

    units

--- unit.h
+++ unit.h
@@ -7,6 +7,14 @@ class Unit {
 public:
     Unit(UnitName u, std::string n, int m, int g, int s, int sm, int t);
 
+    UnitName get_unit() const { return unit; }
+    std::string get_name() const { return name; }
+    int get_min() const { return min; }
+    int get_gas() const { return gas; }
+    int get_sup() const { return sup; }
+    int get_sup_max() const { return sup_max; }
+    int get_time() const { return time; }
+
 private:
     UnitName unit;
 	std::string name;
---
+++ unit_library.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <vector>
+#include "unit.h"
+#include "unit_name.h"
+
+class UnitLibrary {
+public:
+    // operator overload on unitname
+
+private:
+    vector <Unit> units;
+};

5ade776e5cc61fe1dd94d5850e5f8c64ec1e5a15
Oliver Chen <olichen@ucdavis.edu>
2020-12-31 17:26:58 -0800

    added unit handler

---
+++ unit_handler.cpp
@@ -0,0 +1,50 @@
+#include "unit_handler.h"
+
+void UnitHandler::reset() {
+    for (auto u : units)
+        u.clear();
+}
+
+bool UnitHandler::can_build(Unit::UnitName u) {
+    // check prereqs
+    for (auto u : Unit::get_prereq(u))
+        if (units[u].size() == 0)
+            return false;
+    // check builder
+    Unit::UnitName builder = Unit::get_builder(u);
+    for (auto t : units[builder])
+        if (t == 0)
+            return true;
+    return false;
+}
+
+void UnitHandler::build_unit(Unit::UnitName u) {
+    // use unit builder
+    Unit::UnitName builder = Unit::get_builder(u);
+    for (auto t : units[builder])
+        if (t == 0)
+            t = Unit::get_time(u);
+}
+
+void UnitHandler::spawn_unit(Unit::UnitName u) {
+    // add to units
+    units[u].push_back(0);
+}
+
+void UnitHandler::next_frame() {
+    for (auto v : units)
+        for (auto t : v)
+            if (t > 0)
+                t--;
+}
+
+// DEBUG
+#include <iostream>
+
+void UnitHandler::print() {
+    for (int i = 0; i < Unit::UNIT_NULL; i++)
+        for (const auto u : units[i])
+            std::cout << ' ' << i;
+}
+
+int main() {}
---
+++ unit_handler.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <vector>
+#include "unit.h"
+
+class UnitHandler {
+public:
+    UnitHandler() : units (Unit::UNIT_NULL) {}
+    void reset();
+    bool can_build(Unit::UnitName);
+    void build_unit(Unit::UnitName);
+    void spawn_unit(Unit::UnitName);
+
+    void next_frame();
+    void print(); // DEBUG
+
+private:
+    std::vector <std::vector<int>> units; // unitname to current task
+};
